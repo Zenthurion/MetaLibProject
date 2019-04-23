faa377fbe15c2bbb8e1df36a092438aa001ffd30
Markus <markus.mjak@gmail.com>
2019-04-23 16:59:25 +0200

    Update to selections. Interactions removed

--- Interaction/DSSelections.cs
+++ Interaction/DSSelections.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using DwarvenSoftware.Framework.Core;
+using UnityEngine;
 
 namespace DwarvenSoftware.Framework.Interaction
 {
@@ -10,26 +11,44 @@ namespace DwarvenSoftware.Framework.Interaction
 
         public IReadOnlyList<ISelectable> ReadonlySelection => _selection.AsReadOnly();
 
+        protected override void Awake()
+        {
+            base.Awake();
+            _selection = new List<ISelectable>();
+            _previousSelection = new List<ISelectable>();
+        }
+
+
         public void Add(ISelectable selectable)
         {
+            if(_selection.Contains(selectable) || !selectable.IsSelectable) return;
             _selection.Add(selectable);
-            selectable.Select();
+            selectable.OnSelect();
+            
+            Debug.Log("Selected: " + (selectable as MonoBehaviour)?.name);
         }
 
         public void Remove(ISelectable selectable)
         {
+            if(!_selection.Contains(selectable)) return;
             _selection.Remove(selectable);
-            selectable.Deselect();
+            selectable.OnDeselect();
+            
+            Debug.Log("Deselected: " + (selectable as MonoBehaviour)?.name);
         }
 
         public void Clear()
         {
+            if(_selection.Count == 0) return;
+            
             var swap = _previousSelection;
             _previousSelection = _selection;
             _selection = swap;
             _selection.Clear();
             
-            _previousSelection.ForEach((s) => s.Deselect());
+            _previousSelection.ForEach((s) => s.OnDeselect());
+            
+            Debug.Log("Clearing Selection");
         }
 
         public void ReselectPrevious()
@@ -38,7 +57,12 @@ namespace DwarvenSoftware.Framework.Interaction
             _previousSelection = _selection;
             _selection = swap;
 
-            _selection.ForEach((s) => s.Select());
+            _selection.ForEach((s) => s.OnSelect());
+        }
+
+        public bool IsSelected(ISelectable selectable)
+        {
+            return _selection.Contains(selectable);
         }
     }
 }
